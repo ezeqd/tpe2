3e3bb4333c2ddbc14cb2d8a1930578e7755304fa
martinmassimo <martin.massimo.91@gmail.com>
2019-11-13 11:47:51 -0300

    metodo switch para cambiar permisos de usuarios

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -8,19 +8,31 @@ class UserModel {
         $this->db = new PDO('mysql:host=localhost;'.'dbname=tpe2_db;charset=utf8', 'root', '');
     }
 
-    public function GetUsuario($usuario){
+    public function GetUsuario($email){
         //acá irian unos try-catch
         $sentencia = $this->db->prepare("SELECT * FROM usuario WHERE email = ?");
-        $sentencia->execute(array($usuario));
+        $sentencia->execute(array($email));
         
-        $password = $sentencia->fetch(PDO::FETCH_OBJ);
+        $usuario = $sentencia->fetch(PDO::FETCH_OBJ);
         
-        return $password;
+        return $usuario;
+    }
+
+    public function SwitchAdmin ($email){
+        // FALTA PROBARLO
+        $usuario = $this->GetUsuario($email);
+        $sentencia = $this->db->prepare("UPDATE usuario SET admin=? FROM usuario WHERE email=?");
+        if ($usuario->admin){
+            $sentencia->execute(array(0,$usuario));
+        }
+        else{
+            $sentencia->execute(array(1,$usuario));
+        }
     }
 
     public function SetUser($usuario, $pass){
-        $sentencia = $this->db->prepare("INSERT INTO usuario(email, password) VALUES(?,?)");
-        $sentencia->execute(array($usuario,$pass));
+        $sentencia = $this->db->prepare("INSERT INTO usuario(email, password,admin) VALUES(?,?)");
+        $sentencia->execute(array($usuario,$pass,0));
     }
 
     
