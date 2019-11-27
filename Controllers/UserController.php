a8042e29adb408f44da2835f108b0901937e1e71
Ezequiel <ezequiel.domenech@gmail.com>
2019-11-27 20:28:55 -0300

    Agregado modulo de encriptacion del sistema (sin terminar).

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -49,11 +49,11 @@ class UserController {
         if(isset($_POST['lost-user'])){
             $user = $this->model->getUsuario($_POST['lost-user']);
             if ($user){
-                $uniqidStr = md5(uniqid(mt_rand()));
+                $crypt = new Encryption();
                 $receiver = $user->email;
                 $subject = "Recuperar contraseña";
-                $hash = "asd123";
-                $url = BASE_URL . "/showsetpass?id=". $hash;
+                $hash = $crypt->hash($user->id_usuario);
+                $url = BASE_URL . "showsetpass?id=". $hash;
                 $body = "Estimado usuario ingrese al siguiente link para cambiar la contraseña " . $url;
                 $mail = new Mailer($receiver,$subject, $body);
                 $error = $mail->sendMail();
@@ -66,79 +66,6 @@ class UserController {
                 $this->view->DisplayRecovery("Mail no valido");
             }
         }
-//instanciamos un objeto de la clase phpmailer al que llamamos 
-//   //por ejemplo mail
-//  //Definimos las propiedades y llamamos a los métodos 
-//   //correspondientes del objeto mail
-
-//   //Con PluginDir le indicamos a la clase phpmailer donde se 
-//   //encuentra la clase smtp que como he comentado al principio de 
-//   //este ejemplo va a estar en el subdirectorio includes
-//   $mail->PluginDir = "Helpers/PHPMailer";
-
-//   //Con la propiedad Mailer le indicamos que vamos a usar un 
-//   //servidor smtp
-//   $mail->Mailer = "smtp";
-
-//   //Asignamos a Host el nombre de nuestro servidor smtp
-//   $mail->Host = "smtp.gmail.com";
-
-//   //Le indicamos que el servidor smtp requiere autenticación
-//   $mail->SMTPAuth = true;
-
-//   //Le decimos cual es nuestro nombre de usuario y password
-//   $mail->Username = "contacto@cuchillosdetandil.com.ar"; 
-//   $mail->Password = "acero1045";
-
-//   //Indicamos cual es nuestra dirección de correo y el nombre que 
-//   //queremos que vea el usuario que lee nuestro correo
-//   $mail->From = "micuenta@HotPOP.com";
-//   $mail->FromName = "Eduardo Garcia";
-
-//   //el valor por defecto 10 de Timeout es un poco escaso dado que voy a usar 
-//   //una cuenta gratuita, por tanto lo pongo a 30  
-//   $mail->Timeout=30;
-
-//   //Indicamos cual es la dirección de destino del correo
-//   $mail->AddAddress($user->email);
-
-//   //Asignamos asunto y cuerpo del mensaje
-//   //El cuerpo del mensaje lo ponemos en formato html, haciendo 
-//   //que se vea en negrita
-//   $mail->Subject = "Prueba de phpmailer";
-//   $mail->Body = "<b>Mensaje de prueba mandado con phpmailer en formato html</b>";
-
-//   //Definimos AltBody por si el destinatario del correo no admite email con formato html 
-//   $mail->AltBody = "Mensaje de prueba mandado con phpmailer en formato solo texto";
-
-//   //se envia el mensaje, si no ha habido problemas 
-//   //la variable $exito tendra el valor true
-//   $exito = $mail->Send();
-
-//   //Si el mensaje no ha podido ser enviado se realizaran 4 intentos mas como mucho 
-//   //para intentar enviar el mensaje, cada intento se hara 5 segundos despues 
-//   //del anterior, para ello se usa la funcion sleep	
-//   $intentos=1; 
-//   while ((!$exito) && ($intentos < 5)) {
-// 	sleep(5);
-//      	echo $mail->ErrorInfo;
-//      	$exito = $mail->Send();
-//      	$intentos=$intentos+1;	
-	
-//    }
- 
-		
-//    if(!$exito)
-//    {
-// 	echo "Problemas enviando correo electrónico a ".$valor;
-// 	echo "<br/>".$mail->ErrorInfo;	
-//    }
-//    else
-//    {
-// 	echo "Mensaje enviado correctamente";
-//    } 
-//         //aca debo enviar mail al $user
-
     }
 
     public function BorrarUsuario($id){
@@ -155,7 +82,6 @@ class UserController {
             $usuario = $this->model->GetUsuario($email);
         }
         $this->view->DisplayUsuarios($usuarios,$usuario);
-
     }
 }
 ?>
--- Helpers/Encryption.php
+++ Helpers/Encryption.php
@@ -1,2 +1,9 @@
 <?php
 
+class Encryption {
+    CONST SALT = "algo";
+
+    public function hash($id) {}
+
+    public function verify($hash) {return}
+}
