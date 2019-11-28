6785b213788c570cbebdaae7eb5b6638cf7cba18
martinmassimo <martin.massimo.91@gmail.com>
2019-11-27 22:19:36 -0300

    checkeo de parametros para la query de comentarios

--- Helpers/Encryption.php
+++ Helpers/Encryption.php
@@ -3,7 +3,7 @@
 class Encryption {
     CONST SALT = "algo";
 
-    public function hash($id) {}
+    // public function hash($id) {}}
 
-    public function verify($hash) {return}
+    // public function verify($hash) {return}
 }
